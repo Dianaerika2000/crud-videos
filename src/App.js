2135e8d7b5b6d78493d77c6a2fdd064d020bf67f
Diana Montano <dianaerika.montano16@gmail.com>
2022-10-06 12:57:15 -0400

    Cambios en el navBar

--- src/App.js
+++ src/App.js
@@ -24,7 +24,7 @@ const App = () => {
   // options
   let navBarOptions = {
     main: [
-      { option: 'Home', to: '/' },
+      // { option: 'Home', to: '/' },
       { option: 'Videos', to: '/videos' },
     ],
     right: [
